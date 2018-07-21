b4b8ef488726068177b4eafb35bca2e45474c86e
JaneMabe <janemabe@yahoo.com>
2018-07-21 17:52:45 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.name === "susan") {
+if (index.name === "joe") {
     console.log("The name is correct")
 } else if { Joe
     console.log("The name is correct")
@@ -8,7 +8,7 @@ if (index.name === "susan") {
 
 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
